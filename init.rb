0c633994b54c12a86ce38188ca251a75d5ff2c89
Mikhail Mikhailov <m.mikhaylov1@uniyar.ac.ru>
2017-02-09 09:19:41 +0200

    ADD: Plugin Init file

--- init.rb
+++ init.rb
@@ -1,8 +1,21 @@
+require_dependency 'issue'
+require_dependency 'attachment'
+
+# Guards against including the module multiple time (like in tests)
+  # and registering multiple callbacks
+  unless Issue.included_modules.include? IssuePatch
+    Issue.send(:include, IssuePatch)
+  end
+  unless Attachment.included_modules.include? AttachmentPatch
+    Attachment.send(:include, AttachmentPatch)
+  end
+
 Redmine::Plugin.register :redmine_transaction_informer do
   name 'Redmine Transaction Informer plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Mikhailov Mikhail'
+  description 'This is a plugin for Redmine, which sends to foreign URL JSON about transaction data'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/MikhailovMV/redmine_transaction_informer'
+  author_url 'http://profit-lab.top'
+  settings :default => { 'domain' => 'profit-lab.top', 'url' => '/htmls/counter.php'}, :partial => 'settings/plugin'
 end
---
+++ lib/attachment_patch.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+  # Patches Redmine's Issues dynamically.  
+  module AttachmentPatch
+    def self.included(base) # :nodoc:
+      base.extend(ClassMethods)
+
+      base.send(:include, InstanceMethods)
+
+      # Same as typing in the class 
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+        after_save :send_request_to_control
+        after_destroy  :send_request_to_control
+      end
+
+    end
+    
+    module ClassMethods
+    end
+    
+    module InstanceMethods
+      def send_request_to_control
+        Net::HTTP.get('profit-lab.top', '/htmls/counter.php')
+        return true
+      end
+
+    end    
+  end
